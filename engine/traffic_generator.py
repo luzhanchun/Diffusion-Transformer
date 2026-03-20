e5f9d37bee22e8d3ff4e47de60890bdce1b83e1c
lu'com <1030279559@qq.com>
2026-03-20 16:49:45 +0800

    666

--- engine/traffic_generator.py
+++ engine/traffic_generator.py
@@ -148,6 +148,7 @@ class TrafficGenerator:
             print("[INFO] 没有可写入的数据包")
             return
         file_exists = os.path.isfile(filename)
+        print(file_exists)
         # append=True 表示追加写入，不覆盖原文件
         wrpcap(filename, self.packets, append=file_exists)
         print(f"\n[INFO] Completed! 背景流量已保存至: {filename} (共{len(self.packets)} packets)")
